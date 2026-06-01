7ffedf8cc5fde742a1c6beb4723590a7897dba69
Almudena Guerras <almudenaguerraswm@gmail.com>
2026-06-01 12:57:25 +0200

    ♻️ refactor(landing): register web components before landing scripts

--- landing/main.ts
+++ landing/main.ts
@@ -6,14 +6,14 @@ import "../src/stylesheets/global.css";
 import "./styles/landing.css";
 
 /* ==========================================================
+   Web components
+========================================================== */
+import "../dist/landing-challenge/landing-challenge.esm.js";
+
+/* ==========================================================
    Landing scripts
 ========================================================== */
 import "./scripts/mobile-nav";
 import "./scripts/responsive-logo";
 import "./scripts/header-scroll";
 import "./scripts/video-embed";
-
-/* ==========================================================
-   Web components
-========================================================== */
-import "../dist/landing-challenge/landing-challenge.esm.js";
